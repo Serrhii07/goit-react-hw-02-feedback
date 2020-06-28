0d405af0395e37905f4509fc13762c6b4d75a708
Serrhii07 <serge070989@gmail.com>
2020-06-28 13:01:06 +0300

    add type button

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -4,9 +4,15 @@ import styles from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <div className={styles.feedback} onClick={onLeaveFeedback}>
-    <button className={styles.feedback_btn}>{options[0]}</button>
-    <button className={styles.feedback_btn}>{options[1]}</button>
-    <button className={styles.feedback_btn}>{options[2]}</button>
+    <button className={styles.feedback_btn} type="button">
+      {options[0]}
+    </button>
+    <button className={styles.feedback_btn} type="button">
+      {options[1]}
+    </button>
+    <button className={styles.feedback_btn} type="button">
+      {options[2]}
+    </button>
   </div>
 );
 
