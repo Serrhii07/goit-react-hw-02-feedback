beedca97c317aeb919229b53e07fe95d4e8cba52
Serrhii07 <serge070989@gmail.com>
2020-06-28 13:58:59 +0300

    fix FeedbackOptions.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div className={styles.feedback} onClick={onLeaveFeedback}>
-    <button className={styles.feedback_btn} type="button">
-      {options[0]}
-    </button>
-    <button className={styles.feedback_btn} type="button">
-      {options[1]}
-    </button>
-    <button className={styles.feedback_btn} type="button">
-      {options[2]}
-    </button>
-  </div>
+  <ul className={styles.feedback}>
+    {options.map(option => (
+      <li className={styles.feedback_item} key={option}>
+        <button
+          className={styles.feedback_btn}
+          onClick={onLeaveFeedback}
+          type="button"
+        >
+          {option}
+        </button>
+      </li>
+    ))}
+  </ul>
 );
 
 FeedbackOptions.propTypes = {
